1b77fc57875130012e0c90d6e3837826fa784865
Israel R. Curbelo <74841063+OnlineDimension@users.noreply.github.com>
2024-03-16 21:15:59 -0400

    Added Q-learning algorithm

--- FrozenLake.py
+++ FrozenLake.py
@@ -3,24 +3,26 @@ import gymnasium as gym
 import time
 from gymnasium.envs.toy_text.frozen_lake import generate_random_map
 
+rng = np.random.default_rng()
+
 """
 For policy_evaluation, policy_improvement, policy_iteration and valu2wsws:
 
-	P (dict): From gym.core.Environment
-		For each pair of states in [0, nS - 1] and actions in [0, nA - 1], P[state][action] is a
-		list of tuples of the form [(probability, nextstate, reward, terminal),...] where
-			- probability: float
-				the probability of transitioning from "state" to "nextstate" with "action"
-			- nextstate: int
-				denotes the state we transition to (in range [0, nS - 1])
-			- reward: int
-				either 0 or 1, the reward for transitioning from "state" to
-				"nextstate" with "action"
-			- terminal: bool
-			  True when "nextstate" is a terminal state (hole or goal), False otherwise
-	nS (int): number of states in the environment
-	nA (int): number of actions in the environment
-	gamma (float): Discount factor. Number in range [0, 1)
+    P (dict): From gym.core.Environment
+        For each pair of states in [0, nS - 1] and actions in [0, nA - 1], P[state][action] is a
+        list of tuples of the form [(probability, nextstate, reward, terminal),...] where
+            - probability: float
+                the probability of transitioning from "state" to "nextstate" with "action"
+            - nextstate: int
+                denotes the state we transition to (in range [0, nS - 1])
+            - reward: int
+                either 0 or 1, the reward for transitioning from "state" to
+                "nextstate" with "action"
+            - terminal: bool
+              True when "nextstate" is a terminal state (hole or goal), False otherwise
+    nS (int): number of states in the environment
+    nA (int): number of actions in the environment
+    gamma (float): Discount factor. Number in range [0, 1)
 """
 
 """
@@ -72,13 +74,13 @@ def policy_evaluation(P, nS, nA, policy, gamma=0.9, tol=1e-3):
     d = 1
 
     while d > tol:
-    	V_ = np.zeros(nS)
-    	for s in range(nS):
-    		 a = pi[s]
-    		 for p,s_,r,_ in P[s][a]:
-    		 	V_[s] += p*(r+gamma*V[s_])
-    	d = np.max(np.abs(V_ - V))
-    	V = V_
+        V_ = np.zeros(nS)
+        for s in range(nS):
+             a = pi[s]
+             for p,s_,r,_ in P[s][a]:
+                V_[s] += p*(r+gamma*V[s_])
+        d = np.max(np.abs(V_ - V))
+        V = V_
     
     return V
 
@@ -103,20 +105,20 @@ def policy_improvement(P, nS, nA, value_from_policy, policy, gamma=0.9):
     
     for s in range(nS):
     
-    	a_0 = pi[s]
-    	
-    	A = np.zeros(nA)
-    	
-    	for a in range(nA):
-    		temp = 0
-    		for p, s_, r, _ in P[s][a]:
-    			temp += p*(r+gamma*V[s_])
-    		A[a] = temp
-    	pi[s] = np.argmax(A)
-        	
+        a_0 = pi[s]
+        
+        A = np.zeros(nA)
+        
+        for a in range(nA):
+            temp = 0
+            for p, s_, r, _ in P[s][a]:
+                temp += p*(r+gamma*V[s_])
+            A[a] = temp
+        pi[s] = np.argmax(A)
+            
     return pi
 
-
+#0X0
 # policy iteration
 
 
@@ -142,14 +144,14 @@ def policy_iteration(P, nS, nA, gamma=0.9, tol=1e-3):
     flag = True
     
     while flag:
-    	V = policy_evaluation(P,nS,nA,pi,gamma,tol)
-    	pi_ = policy_improvement(P, nS, nA, V, pi, gamma)
-    	d = pi_ - pi
-    	
-    	if np.linalg.norm(d) < tol:
-    		flag = False
-    	pi = pi_
-   	
+        V = policy_evaluation(P,nS,nA,pi,gamma,tol)
+        pi_ = policy_improvement(P, nS, nA, V, pi, gamma)
+        d = pi_ - pi
+        
+        if np.linalg.norm(d) < tol:
+            flag = False
+        pi = pi_
+    
     return V, pi
 
 
@@ -176,60 +178,108 @@ def value_iteration(P, nS, nA, gamma=0.9, tol=1e-3):
     
     d = 1
     while d >= tol:
-    	V_ = np.zeros(nS)
-    	for s in range(nS):
-    		A = np.zeros(nA)
-    		for a in range(nA):
-    			for p,s_,r,_ in P[s][a]:
-    				A[a] += p*(r+gamma*V[s_])
-    		V_[s] = max(A)
-    	d = np.linalg.norm(V_ - V)
-    	V = V_
+        V_ = np.zeros(nS)
+        for s in range(nS):
+            A = np.zeros(nA)
+            for a in range(nA):
+                for p,s_,r,_ in P[s][a]:
+                    A[a] += p*(r+gamma*V[s_])
+            V_[s] = max(A)
+        d = np.linalg.norm(V_ - V)
+        V = V_
     
     for s in range(nS):
-    	A = np.zeros(nA)
-    	for a in range(nA):
-    		for p,s_,r,_ in P[s][a]:
-    			A[a] += p*(r+gamma*V[s_])
-    		Amax = np.argmax(A)
-    	pi[s] = Amax
+        A = np.zeros(nA)
+        for a in range(nA):
+            for p,s_,r,_ in P[s][a]:
+                A[a] += p*(r+gamma*V[s_])
+            Amax = np.argmax(A)
+        pi[s] = Amax
        
     return V, pi
 
-def render_single(env, pi, max_steps=1000):
+def q_learning(env, nS, nA, gamma=0.9, alpha=0.5, epsilon=0.5, max_steps=100):
+    """
+    Learn Q function and policy by using Q learning for a given
+    gamma, alpha, epsilon and environment
 
-	R = 0
-	obs, info = env.reset()
-	for _ in range(max_steps):
-		env.render()
-		#time.sleep(0.25)
-		a = pi[obs]  
-		obs, r, terminated, truncated, info = env.step(a)
-		R += r
+    Args:
+            env, nS, nA, gamma: defined at beginning of file
+            alpha: learning rate
+            epsilon: for epsilon greedy algorithm
+            tol (float): Terminate value iteration when
+                            max |value_function(s) - prev_value_function(s)| < tol
 
-		if terminated or truncated:
-			break
-	env.render()
-	if not (terminated or truncated):
-		print("Anna did not reach a terminal state in {} steps.".format(max_steps))
-	else:
-		print("Episode reward: %f" % R)
+    Returns:
+            Q (np.ndarray([nS,nA])): Q function resulting from Q learning
+            pi (np.ndarray([nS])): policy resulting from Q iteration
+
+    """
+
+    s, _ = env.reset()
+    Q = np.zeros([nS,nA])
+    pi = np.zeros(nS, dtype=int)
+
+    for _ in range(max_steps):
+        coin = rng.random()
+        if coin < 1 - epsilon:
+            a = np.argmax(Q[s])
+        else:
+            a = rng.integers(4)
+        s_, r, terminated, truncated, _ = env.step(a) 
+        Q[s,a] = (1-alpha)*Q[s,a] + alpha*(r+gamma*np.max(Q[s_]))
+        s = s_
+        if truncated or terminated:
+            s, _ = env.reset()
+
+    for s in range(nS):
+        pi[s] = np.argmax(Q[s])
+       
+    return Q, pi
+
+def render_single(env, pi, max_steps=50):
+
+    R = 0
+    obs, info = env.reset()
+    for _ in range(max_steps):
+        env.render()
+        time.sleep(0.25)
+        a = pi[obs]  
+        obs, r, terminated, truncated, info = env.step(a)
+        R += r
+
+        if terminated or truncated:
+            break
+    #time.sleep(20)
+    env.render()
+    if not (terminated or truncated):
+        print("Anna did not reach a terminal state in {} steps.".format(max_steps))
+    else:
+        print("Episode reward: %f" % R)
+    
 
 if __name__ == "__main__":
-	
-    """
-	d = ['SFHFG',
-		 'FFHFF',
-		 'FFHFF',
-		 'FFHFF',
-		 'FFFFF']
-	"""
+    
+    '''
+    d2 = ['SFHFG',
+         'FFHFF',
+         'FFHFF',
+         'FFHFF',
+         'FFFFF']
+    '''
+    d = ['HHHHHHHH',
+         'HSFFFFFG',
+         'HHHHHHHH']
+    
+    env = gym.make('FrozenLake-v1', desc=d, is_slippery=False, render_mode="human")
 
-    env = gym.make('FrozenLake-v1', desc=generate_random_map(size=9), is_slippery=True, render_mode="human")
+    #V_pi, p_pi = policy_iteration(env.P, env.observation_space.n, env.action_space.n)
+    #render_single(env, p_pi)
 
+    #V_vi, p_vi = value_iteration(env.P, env.observation_space.n, env.action_space.n)
+    #render_single(env, p_vi)
 
-	#V_pi, p_pi = policy_iteration(env.P, env.observation_space.n, env.action_space.n)
-	#render_single(env, p_pi)
-
-    V_vi, p_vi = value_iteration(env.P, env.observation_space.n, env.action_space.n)
-    render_single(env, p_vi)
+    Q, p = q_learning(env, env.observation_space.n, env.action_space.n) 
+    print(Q)
+    print(p)
+    #render_single(env,p)
